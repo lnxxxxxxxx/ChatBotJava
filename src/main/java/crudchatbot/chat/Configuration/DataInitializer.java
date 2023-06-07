4f9a0971b3403f1d624f94d42f5cfcd46d32c3cb
lnxxxxxxxx <leandro.luna25@hotmail.com>
2023-06-07 01:20:41 -0300

    new person

--- src/main/java/crudchatbot/chat/Configuration/DataInitializer.java
+++ src/main/java/crudchatbot/chat/Configuration/DataInitializer.java
@@ -21,21 +21,21 @@ public class DataInitializer implements CommandLineRunner {
     chatbot1.setDni(11111111L);
     chatbot1.setNombre("Usuario1");
     chatbot1.setConsumo("Su consumo es de 4000 kWh");
-    chatbot1.setPrecio(19.503f);
+    chatbot1.setPrecio(19000.503f);
     chatbotRepository.save(chatbot1);
 
     Chatbot chatbot2 = new Chatbot();
     chatbot2.setDni(98765432L);
     chatbot2.setNombre("Usuario2");
     chatbot2.setConsumo("Su consumo es de 3000 kWh");
-    chatbot2.setPrecio(15.000f);
+    chatbot2.setPrecio(15000.000f);
     chatbotRepository.save(chatbot2);
 
     Chatbot chatbot3 = new Chatbot();
     chatbot3.setDni(12345678L);
     chatbot3.setNombre("Usuario3");
     chatbot3.setConsumo("Su consumo es de 5000 kWh");
-    chatbot3.setPrecio(22.000f);
+    chatbot3.setPrecio(22000.000f);
     chatbotRepository.save(chatbot3);
   }
 }
